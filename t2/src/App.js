ae05fd9bebc1e1084f80f7553b5e105c54031f6d
choudharyumar <choudharyumar948@gmail.com>
2023-08-16 10:46:06 +0500

    1

--- t2/src/App.js
+++ t2/src/App.js
@@ -1,8 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import User from "./Component";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <h1>Hello world </h1>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+      <User></User>
+    </div>
+  );
 }
 
 export default App;
---
+++ t2/src/Component.js
@@ -0,0 +1,10 @@
+// functional component
+function User() {
+  return (
+    <div>
+      <h1>Hello here is a functional component</h1>
+      <h1>there is addition of a tag in component</h1>
+    </div>
+  );
+}
+export default User;
