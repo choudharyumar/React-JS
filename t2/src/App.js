386cb21e8f11d37a26fefe236403faa032a71c1e
choudharyumar <choudharyumar948@gmail.com>
2023-08-16 12:05:42 +0500

    1

--- t2/src/App.js
+++ t2/src/App.js
@@ -1,16 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import User from "./Component";
-
+import Xlass from "./Class";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world </h1>
-      <User></User>
-      <User></User>
-      <User></User>
-      <User></User>
-      <User></User>
+      {/* <h1>Hello world </h1> */}
+      {/* <User></User> */}
+      <Xlass></Xlass>
     </div>
   );
 }
---
+++ t2/src/Class.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+export default class Xlass extends Component {
+  render() {
+    return <h1> hello here from class component</h1>;
+  }
+}
