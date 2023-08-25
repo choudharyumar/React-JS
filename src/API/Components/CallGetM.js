4c68fc98bc038753b7c707c65b0bbe766e1e7f48
choudharyumar <choudharyumar948@gmail.com>
2023-08-25 14:43:10 +0500

    Update CallGetM.js

--- src/API/Components/CallGetM.js
+++ src/API/Components/CallGetM.js
@@ -3,14 +3,34 @@ import { Table } from "react-bootstrap";
 
 const CallGetM = () => {
   const [data, setdata] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://fakestoreapi.com/products?limit=5").then((result) => {
+  //       result.json().then((resp) => {
+  //         //   console.log(resp);
+  //         setdata(resp);
+  //       });
+  //     });
+  //   }, []);
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products?limit=5").then((result) => {
-      result.json().then((resp) => {
-        //   console.log(resp);
-        setdata(resp);
-      });
-    });
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products?limit=5"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        setdata(data);
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    };
+
+    fetchData();
   }, []);
+
   console.log(data);
   return (
     <div>
@@ -18,14 +38,16 @@ const CallGetM = () => {
       <Table striped bordered hover>
         <tr>
           <td>id</td>
-          <td>tittle</td>
+          <td>title</td>
         </tr>
 
         {data.map((item) => {
-          <tr>
-            <td>{item.id}</td>
-            <td>{item.tittle}</td>
-          </tr>;
+          return (
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+            </tr>
+          );
         })}
       </Table>
     </div>
